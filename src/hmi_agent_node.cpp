cf2591c87a4b5d9b6359e7da24a9fdd82687c978
frc195programming <frc195programming@gmail.com>
2022-02-08 00:57:02 +0000

    Allison: Basic Signals

--- src/hmi_agent_node.cpp
+++ src/hmi_agent_node.cpp
@@ -5,7 +5,7 @@
 #include <string>
 #include <mutex>
 #include <rio_control_node/Joystick_Status.h>
-
+#include <hmi_agent_node/HMI_Signals.h>
 #include <action_helper/action_helper.hpp>
 
 #define RATE (100)
@@ -34,36 +34,87 @@ uint8_t debounce(int index, uint8_t value)
 }
 
 
-void joystick_status_callback( const rio_control_node::Joystick_Status& joytick_status )
+void joystick_status_callback( const rio_control_node::Joystick_Status& joystick_status )
 {
-
-    // we will parse Joystick 0 buttons 0/1/2/3
-
-    if( joytick_status.joysticks.size() <= 0){ return; }
-    if( joytick_status.joysticks[0].buttons.size() <= 4){ return; }
-
-    const rio_control_node::Joystick* current_joystick = &joytick_status.joysticks[0];
-
-    if( debounce(0, current_joystick->buttons[0]) == 1 )
+    static double turret_aim_degrees = 0;
+    static double turret_hood_degrees = 0;
+    static double turret_speed_rpm = 0;
+    hmi_agent_node::HMI_Signals output_signals;
+    
+    if (joystick_status.joysticks[0].buttons[0])//near
     {
-        std::cout << "MOVE PLS\n";
-        action_helper->req_single_action("TestAutonomous", "MoveForward");
+        turret_aim_degrees = 0;
+        turret_hood_degrees = 0;
+        turret_speed_rpm = 0;
+    }
+    if (joystick_status.joysticks[0].buttons[0])//near mid
+    {
+        turret_aim_degrees = 0;
+        turret_hood_degrees = 0;
+        turret_speed_rpm = 0;
+    }
+    if (joystick_status.joysticks[0].buttons[0])//mid
+    {
+        turret_aim_degrees = 0;
+        turret_hood_degrees = 0;
+        turret_speed_rpm = 0;
+    }
+    if (joystick_status.joysticks[0].buttons[0])//mid far
+    {
+        turret_aim_degrees = 0;
+        turret_hood_degrees = 0;
+        turret_speed_rpm = 0;
+    }
+    if (joystick_status.joysticks[0].buttons[0])//far
+    {
+        turret_aim_degrees = 0;
+        turret_hood_degrees = 0;
+        turret_speed_rpm = 0;
     }
 
-    if( debounce(1, current_joystick->buttons[1]) == 1 )
+    if (joystick_status.joysticks[0].buttons[0])//aim left
     {
+        turret_aim_degrees -= 0;
+    }
+    if (joystick_status.joysticks[0].buttons[0])//aim right
+    {
+        turret_aim_degrees += 0;
     }
 
-    if( debounce(2, current_joystick->buttons[2]) == 1 )
+
+    if (joystick_status.joysticks[0].buttons[0])//hood up
     {
+       turret_hood_degrees -= 0; 
+    }
+    if (joystick_status.joysticks[0].buttons[0])//hood down
+    {
+       turret_hood_degrees += 0;
     }
 
-    if( debounce(3, current_joystick->buttons[3]) == 1 )
+
+    if (joystick_status.joysticks[0].buttons[0])//speed up
     {
-        std::cout << "CANCEL Action\n";
-        action_helper->req_cancel_category("testingActions");
-        action_helper->req_cancel_category("TestAutonomous");
+        turret_speed_rpm -= 0;
     }
+    if (joystick_status.joysticks[0].buttons[0])//speed down
+    {
+        turret_speed_rpm += 0;
+    }
+
+
+
+
+
+    output_signals.drivetrain_brake = joystick_status.joysticks[0].buttons[0];
+    output_signals.drivetrain_fwd_back = joystick_status.joysticks[0].axes[0];
+    output_signals.drivetrain_left_right = joystick_status.joysticks[0].axes[0];
+    output_signals.drivetrain_quickturn = joystick_status.joysticks[0].buttons[0];
+    output_signals.turret_aim_degrees = turret_aim_degrees;
+    output_signals.turret_hood_degrees = turret_hood_degrees;
+    output_signals.turret_speed_rpm = turret_speed_rpm;
+
+    static ros::Publisher signal_publisher = node->advertise<hmi_agent_node::HMI_Signals>("/HMI_Signals",10);
+
 }
 
 int main(int argc, char **argv)
