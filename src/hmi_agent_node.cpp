83740458abed149c8937d5b5e998b47f010012c5
= <=>
2022-03-19 19:34:13 +0000

    Update hmi

--- src/hmi_agent_node.cpp
+++ src/hmi_agent_node.cpp
@@ -135,6 +135,11 @@ void joystick_status_callback(const rio_control_node::Joystick_Status &joystick_
     output_signals.drivetrain_fwd_back = -drive_joystick->getFilteredAxis(1, 0.12) * 0.7;
     double turn = drive_joystick->getFilteredAxis(4, 0.12);
     output_signals.drivetrain_left_right = ck::math::signum(turn) * std::pow(turn, 2);
+    if (drive_joystick->getAxisActuated(3, 0.35))
+    {
+        output_signals.drivetrain_left_right *= 0.4;
+        output_signals.drivetrain_fwd_back *= 0.4;
+    }
     output_signals.drivetrain_quickturn = drive_joystick->getAxisActuated(2, 0.35);
     output_signals.turret_aim_degrees = turret_aim_degrees;
     output_signals.turret_hood_degrees = turret_hood_degrees;
